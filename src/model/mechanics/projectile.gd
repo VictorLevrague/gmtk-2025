8ca87db1c0b2eba2cb332a0ca64f6b4f02515eee
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-31 15:31:29 +0200

    Projectile with particle effect

---
+++ src/model/mechanics/projectile.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+var direction: Vector2 = Vector2.RIGHT
+var speed: float = 100
+var damage: float = 1
+
+func _physics_process(delta: float) -> void:
+    position += direction * speed * delta
