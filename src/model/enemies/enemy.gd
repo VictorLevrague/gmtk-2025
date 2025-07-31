880fe224253410f72446649d8bca6dacfa7ccc5b
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-31 15:50:54 +0200

    Knockback on enemies

--- src/model/enemies/enemy.gd
+++ src/model/enemies/enemy.gd
@@ -9,8 +9,18 @@ class_name enemy
         if health == 0:
             queue_free()
 @export var damage: float = 10
+var knockback: Vector2
+var knockback_strength = 80
 
 func _physics_process(delta: float) -> void:
     var center_screen = get_viewport_rect().size / 2.0
     velocity = (center_screen - position).normalized() * speed
-    move_and_collide(velocity * delta)
+    print("knockback: ", knockback)
+    add_knockback()
+    var collider = move_and_collide(velocity * delta)
+    if collider:
+        collider.get_collider().knockback = (collider.get_collider().global_position - global_position).normalized() * knockback_strength
+
+func add_knockback():
+    knockback = knockback.move_toward(Vector2.ZERO, 1)
+    velocity += knockback
