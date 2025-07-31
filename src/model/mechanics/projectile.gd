802a5fb80453e0388d3ef874abea3186a692d01e
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-07-31 22:32:48 +0200

    Solved pause bug

--- src/model/mechanics/projectile.gd
+++ src/model/mechanics/projectile.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 var direction: Vector2 = Vector2.RIGHT
 var speed: float = 100
-var damage: float = 1
+var damage: float = 5
 
 func _physics_process(delta: float) -> void:
     position += direction * speed * delta
