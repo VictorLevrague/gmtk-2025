67941c4f17658d8056de0bc56d5e15aa65ea5fb4
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-08-01 14:11:12 +0200

    TEmporay test to solve bug of wring projectile direction when look_at

--- src/model/enemies/enemy.gd
+++ src/model/enemies/enemy.gd
@@ -20,8 +20,13 @@ var knockback_strength = 40
 
 func _physics_process(delta: float) -> void:
     var center_screen = get_viewport_rect().size / 2.0
-    #look_at(center_screen)
-    velocity = (center_screen - position).normalized() * speed
+    velocity = (center_screen - global_position).normalized() * speed
+    #if %Sprite2D:
+        #%Sprite2D.flip_v = false if velocity.x > 0.1 else true
+    #%Sprite2D.look_at(center_screen)
+    #if %CollisionShape2D:
+        #%CollisionShape2D.look_at(center_screen)
+        #%CollisionShape2D.rotation_degrees += 90
     add_knockback()
     var collider = move_and_collide(velocity * delta)
     if collider:
