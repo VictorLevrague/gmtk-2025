28a3cd6a763fc0fb910e6ef4745171c9acac1532
VictorLevrague <108005152+VictorLevrague@users.noreply.github.com>
2025-08-03 15:53:00 +0200

    New loose screen and tween activated even when game paused

--- src/ui/menus/close_button.gd
+++ src/ui/menus/close_button.gd
@@ -9,6 +9,7 @@ func _on_mouse_exited() -> void:
 func bounce_scale_tween(scale_value: Vector2, color_value) -> Tween:
     var bounce_duration = 1.
     var tween = get_tree().create_tween().set_trans(Tween.TRANS_SPRING).set_ease(Tween.EASE_OUT)
+    tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
     tween.tween_property(self, "scale", scale_value, bounce_duration)
     tween.set_parallel()
     tween.tween_property(self, "modulate", Color(color_value/255, 1, 1), bounce_duration)
